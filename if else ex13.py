43667bfbf239823ddc88146722daf7fc3cf2e003
aswinb674 <aswinb674@gmail.com>
2025-04-04 14:16:02 +0530

    alter the if else ex13 program

--- if else ex13.py
+++ if else ex13.py
@@ -1,6 +1,10 @@
 salary=int(input("enterr the salary: "))
 age=int(input("enterr the age: "))
 if(salary>=20000 or age<=25):
-    print("you are eligiblr for loan")
+    loan=int(input("enter the loan amount: "))
+    if(loan<=50000):
+        print("you are eligiblr for loan")
+    else:
+        print("maximum loan amount is the 50000")
 else:
     print("you are not eligiblr for loan")
