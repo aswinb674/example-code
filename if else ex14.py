67f616a02b44eafb62fe015f738c995a30992875
aswinb674 <aswinb674@gmail.com>
2025-04-04 14:42:54 +0530

    add if else ex14.py

---
+++ if else ex14.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the a subject mark: "))
+b=int(input("Enter the b subject mark: "))
+c=int(input("Enter the c subject mark: "))
+d=int(input("Enter the d subject mark: "))
+e=int(input("Enter the e subject mark: "))
+f=a+b+c+d+e
+g=f/5
+if(g>=35):
+    print("good student")
+else:
+    print("bad student")
